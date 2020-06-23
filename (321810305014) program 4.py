7e6b8a31e535848685bafadaef3581f0d3e796f7
rajeshreddychintha <67116158+rajeshreddychintha@users.noreply.github.com>
2020-06-23 08:45:54 +0530

    Add files via upload

---
+++ (321810305014) program 4.py
@@ -0,0 +1,13 @@
+import sys
+randomList = ['r', 5, 8]
+
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1/int(entry)
+        break
+    except:
+        print("Oops!", sys.exc_info()[0], "occurred.")
+        print("Next entry.")
+        print()
+print("The reciprocal of", entry, "is", r)
